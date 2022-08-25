8daa8ea96ec12dbf07c3428c41532aa3339bcea8
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-25 14:15:07 +0300

    extracted the Dynamic Path Segment Data

--- pages/portofolio/[projectid].js
+++ pages/portofolio/[projectid].js
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
+
 function PortofolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  // send a request to some backend server
+  // to fetch the piece of data with an id of router.query.[slug]
+
   return (
     <div>
       <h1>The Portofolio Project Page</h1>
